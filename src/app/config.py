af5612dd74bbb9b79866c0871e660322c3cc6344
be-rock <brock.secondary@gmail.com>
2022-02-27 21:36:03 -0600

    restructure config

---
+++ src/app/config.py
@@ -0,0 +1,55 @@
+import datetime
+import os
+from typing import Union
+
+import tomli as toml
+from pydantic import BaseModel, BaseSettings
+
+APP_ENV = os.environ.get("APP_ENV", "dev") # dev, tst, prd
+DEBUG = True if os.environ.get("DEBUG", "").lower().startswith("y") else False
+
+#
+# this sample pydantic model is based on a sample TOML file provided in the
+# official TOML docs https://toml.io/
+#
+class Owner(BaseModel):
+    name: str
+    dob: datetime.datetime
+
+class Database(BaseModel):
+    enabled: bool
+    ports: list[int]
+    data: list[Union[list[str], list[int]]]
+    temp_targets: dict[str, float]
+
+class ServerDetail(BaseModel):
+    ip: str
+    role: str
+
+class Servers(BaseModel):
+    alpha: ServerDetail
+    beta: ServerDetail
+
+class Config(BaseSettings):
+    title: str
+    owner: Owner
+    database: Database
+    servers: Servers
+    class Config:
+        allow_mutation = False
+
+def read_app_config_file(config_file: str = "app-conf.toml") -> dict:
+    with open(config_file, "rb") as conf:
+        return toml.load(conf)
+
+
+def set_app_env_vars(env_file: str = ".env") -> None:
+    with open(env_file, 'r') as f:
+        data = f.read()
+        for item in data.rstrip('\n').split('\n'):
+            k, v = item.split('=')
+            os.environ[k] = v
+
+_conf = read_app_config_file(config_file='/tmp/app-conf.toml')
+CONFIG = Config(**_conf)
+set_app_env_vars(env_file='/tmp/.env')
